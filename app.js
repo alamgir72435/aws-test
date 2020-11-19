a6b5fc022baac19a059c5d07eee33b22a5a20b57
Alamgir Ahamed Saif <alamgir72435@gmail.com>
2020-11-19 20:48:19 +0600

    mongodb connted

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+let uri = 'mongodb+srv://saif:saif@cluster0.szwmr.mongodb.net/awstest?retryWrites=true&w=majority'
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:true
+}).then(() => {
+    console.log('Mongodb Connected')
+}).catch(()=> console.log('Not Connected'))
 
-app.get('/', (req, res) => {
-    res.send(`<h1 style='text-align:center;font-size:45px;color:coral;font-family:Arial, Helvetica, sans-serif;'>Hello baby => i am here</h1>`)
+const todoSchema = new mongoose.Schema({
+    todo:{type:String},
+    isDone:{type:Boolean, default:false}
+})
+
+let Todo = mongoose.model('todos', todoSchema)
+
+const addTodo = async() => {
+    try {
+        await Todo.deleteMany()
+        await Todo.insertMany([
+            {todo:'todo one'},
+            {todo:'todo Two'},
+            {todo:'todo Three'},
+        ])
+    } catch (error) {
+        console.log('Not Posible')
+    }
+}
+addTodo()
+
+app.get('/', async(req, res) => {
+   
+    try {
+        
+        let todos = await Todo.find()
+        res.json(todos)
+
+    } catch (error) {
+        res.send(`<h1 style='text-align:center;font-size:45px;color:coral;font-family:Arial, Helvetica, sans-serif;'>Hello baby =>  Cant do this</h1>`)
+    }
 })
 
 
